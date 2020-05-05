020303526d22dc2c594f17af3aed153dacd26fde
ansible <ansible@localhost.localdomain>
2020-05-05 08:25:18 -0400

    Changed AMI from RHEL7 to Ubuntu 18.

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-098f16afa9edf40be"
+  ami           = "ami-085925f297f89fce1"
   instance_type = "t2.micro"
 
   tags = {
